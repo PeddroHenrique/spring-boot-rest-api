52b499e50c06d02ba71cc648fcb1a549653be53c
Pedro Henrique Brandão Santos <pedro080-henrique@hotmail.com>
2025-04-08 23:15:26 -0300

    Arrumando o problema na criação das roles

--- src/main/java/br/com/springbootrestapi/SpringBootRestApiApplication.java
+++ src/main/java/br/com/springbootrestapi/SpringBootRestApiApplication.java
@@ -49,14 +49,17 @@ public class SpringBootRestApiApplication implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
-        
-        Role adminRole = new Role();
-        adminRole.setName("ROLE_ADMIN");
-        roleRepository.save(adminRole);
-        
-        Role userRole = new Role();
-        userRole.setName("ROLE_USER");
-        roleRepository.save(userRole);
+        if (roleRepository.findByName("ROLE_ADMIN").isEmpty()) {
+            Role adminRole = new Role();
+            adminRole.setName("ROLE_ADMIN");
+            roleRepository.save(adminRole);
+        }
+
+        if (roleRepository.findByName("ROLE_USER").isEmpty()) {
+            Role userRole = new Role();
+            userRole.setName("ROLE_USER");
+            roleRepository.save(userRole);
+        }
     }
 
 }
